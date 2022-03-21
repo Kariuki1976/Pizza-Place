e06436e3994f6e3b51288bf35de059f7a044b021
Kariuki1976 <emmanuel.mwangi@student.moringaschool.com>
2022-03-21 23:58:11 +0300

    add background images to Image folder

---
+++ js-folder/mains.js
@@ -0,0 +1,85 @@
+< div class = "slider-area" >
+    <
+    div class = "bd-example" >
+    <
+    div id = "carouselExampleCaptions"
+class = "carousel-slide"
+data - ride = "carousel" > -- >
+    <
+    ol class = "carousel-indicators" >
+    <
+    li data - target = "#carouselExampleCaptions"
+data - slide - to = "0"
+class = "active" > < /li> <
+    li data - target = "#carouselExampleCaptions"
+data - slide - to = "1" > < /li> <
+    li data - target = "#carouselExampleCaptions"
+data - slide - to = "2" > < /li> <
+    /ol> <
+    div class = "carousel-inner" >
+    <
+    div class = "carousel-item active" >
+    <
+    img src = "./assets/testimony1.jpeg"
+class = "d-block-w-100"
+alt = "..."
+height = "" >
+    <
+    div class = "carousel-caption d-none d-md-block" >
+    <
+    h4 > Wangui Thoithi < /h4> <
+    h5 > Designer < /h5> <
+    p > PizzaTam is the place to be! < /p> <
+    /div> <
+    /div> <
+    div class = "carousel-item" >
+    <
+    img src = "./assets/testimony4.jpeg"
+class = "d-block-w-100"
+alt = "..."
+height = "" >
+    <
+    div class = "carousel-caption d-none d-md-block" >
+    <
+    h4 > William Murage < /h4> <
+    h5 > Developer < /h5> <
+    p > Great Pizza!Great customer service!Great prices! < /p> <
+    /div> <
+    /div> <
+    div class = "carousel-item" >
+    <
+    img src = "./assets/testimony5.jpeg"
+class = "d-block-w-100"
+alt = "..."
+height = "" >
+    <
+    div class = "carousel-caption d-none d-md-block" >
+    <
+    h4 > Wairimu King 'ori</h4> <
+    h5 > Content Creator < /h5> <
+    p > Its everything
+for me! < /p> <
+    /div> <
+    /div> <
+    /div> <
+    a class = "carousel-control-prev"
+href = "#carouselExampleCaptions"
+role = "button"
+data - slide = "prev" >
+    <
+    span class = "carousel-control-prev-icon"
+aria - hidden = "true" > < /span> <
+    span class = "sr-only" > Previous < /span> <
+    /a> <
+    a class = "carousel-control-next"
+href = "#carouselExampleCaptions"
+role = "button"
+data - slide = "next" >
+    <
+    span class = "carousel-control-next-icon"
+aria - hidden = "true" > < /span> <
+    span class = "sr-only" > Next < /span> <
+    /a> <
+    /div> <
+    /div> <
+    /div>
